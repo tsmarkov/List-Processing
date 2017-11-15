163a9acb5e929dff6d92b84500c51aff5de70c42
Tsvetomir Markov <markovcvetomir@gmail.com>
2017-11-15 12:46:22 +0200

    Add main method logic

--- src/listProcessing/Main.java
+++ src/listProcessing/Main.java
@@ -1,4 +1,5 @@
 package listProcessing;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,15 +7,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-	public static void main() throws IOException {
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		List<String> theList = Arrays.asList(reader.readLine().split("\\s+"));
-		
-		String[] line;
-		while ((line = reader.readLine().split("\\s+")) != null && !line[0].equals("end")) {
-			
-			
-		}
-	}
+    public static void main() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<String> theList = Arrays.asList(reader.readLine().split("\\s+"));
+
+
+        while (true) {
+            String[] commands = reader.readLine().split("\\s+");
+
+            if (commands[0].equalsIgnoreCase("end")){
+                break;
+            }
+
+            //interpretCommand(commands, theList);
+        }
+    }
 }
