e7d79690613523350bb4dddefe2492ebcbe1dfba
yangra <brambazluk@gmail.com>
2017-11-15 13:19:46 +0200

    Switch case main

--- src/listProcessing/Main.java
+++ src/listProcessing/Main.java
@@ -6,15 +6,39 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-	public static void main() throws IOException {
+	public static void main(String[] args) throws IOException {
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		List<String> theList = Arrays.asList(reader.readLine().split("\\s+"));
 		
 		String[] line;
 		while ((line = reader.readLine().split("\\s+")) != null && !line[0].equals("end")) {
+
+			interpretCommand(line, theList);
 			
-			
+		}
+	}
+
+	private static void interpretCommand(String[] input, List<String> theList) {
+		switch(input[0].toLowerCase()){
+			case "append":
+				break;
+			case "prepend":
+				break;
+			case "reverse":
+				break;
+			case "insert":
+				break;
+			case "delete":
+				break;
+			case "roll":
+				break;
+			case "sort":
+				break;
+			case "count":
+				break;
+			case "end":
+				break;
 		}
 	}
 }
