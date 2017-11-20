1a8ed7d3097a1d3f2c5495a6f6ffccb120bbae26
Tsvetomir Markov <markovcvetomir@gmail.com>
2017-11-20 21:26:31 +0200

    First input line fix and test current functionality

--- src/listProcessing/Main.java
+++ src/listProcessing/Main.java
@@ -7,13 +7,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        List<String> theList = new ArrayList(Arrays.asList(reader.readLine().split("\\s+")));
+
+        List<String> theList = new ArrayList<>();
+        Collections.addAll(theList, reader.readLine().split("\\s+"));
 
 
         while (true) {
